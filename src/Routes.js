f11c05061da7a9dc6a188fa2df57446ce34bebf4
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-07 23:12:40 +0100

    setup the linter

---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+// Route
+const Routes = () => (
+  <BrowserRouter>
+    <Switch />
+  </BrowserRouter>
+);
+
+export default Routes;
