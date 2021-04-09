765b83b24ddb2a820c680d743b215e9de4dabc72
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-09 22:45:47 +0100

    add login page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import Login from './containers/Login';
+import Login from './containers/Login';
 import Welcome from './components/Welcome';
+import Navbar from './containers/Navbar';
 
 const Routes = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
       <Route exact path="/" component={Welcome} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
 );
