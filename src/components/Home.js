74b751976633f503619cd4d1578f8e693aa091eb
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-12 22:57:19 +0100

    add textes in the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 const Home = () => (
-  <div>Bienvenue mahmoud</div>
+  <div>
+    <h1>
+      Bienvenue mahmoud
+    </h1>
+    <h2>Acte d&apos;urbanisme</h2>
+    <p>Certaficat de Morcellement</p>
+    <p>Certaficat de lotir</p>
+    <p>permis de demolir</p>
+    <p>Permis de construire</p>
+    <p>Certaficat de Conformetè</p>
+    <p>Certaficat d&apos;Urbanisme</p>
+  </div>
 );
 
 export default Home;
